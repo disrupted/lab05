552cca900e01d3b64f522aa8a9468fee6b33a534
disrupted <salomon.popp@gmail.com>
2017-05-09 16:58:22 +0200

    project structure

---
+++ src/AlgorithmAnalysis.java
@@ -0,0 +1,42 @@
+public class AlgorithmAnalysis {
+  public static void main(String[] args) {
+    int sum = 0;
+    int n = 0;
+    // Fragment #1
+    for ( int i = 0; i < n; i ++)
+      sum++;
+
+    // Fragment #2
+    for ( int i = 0; i < n; i ++)
+      for ( int j = 0; j < n; j ++)
+        sum++;
+
+    // Fragment #3
+    for ( int i = 0; i < n; i ++)
+      for ( int j = i; j < n; j ++)
+        sum++;
+
+    // Fragment #4
+    for ( int i = 0; i < n; i ++)
+      sum ++;
+    for ( int j = 0; j < n; j ++)
+      sum ++;
+
+    // Fragment #5
+    for ( int i = 0; i < n; i ++)
+      for ( int j = 0; j < n*n; j ++)
+        sum++;
+
+    // Fragment #6
+    for ( int i = 0; i < n; i ++)
+      for ( int j = 0; j < i; j ++)
+        sum++;
+
+    // Fragment #7
+    for ( int i = 1; i < n; i ++)
+      for ( int j = 0; j < n*n; j ++)
+        if (j % i == 0)
+          for (int k = 0; k < j; k++)
+            sum++;
+  }
+}
