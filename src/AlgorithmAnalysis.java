fab350c7c6bcfb61a281564aa8b9c23ce4d8858b
deniz36 <d.ilyasoglu@outlook.de>
2017-05-13 20:21:11 +0200

    extendet Methods

--- src/AlgorithmAnalysis.java
+++ src/AlgorithmAnalysis.java
@@ -1,81 +1,140 @@
 public class AlgorithmAnalysis {
-  int n = 10;
 
-  public static void main(String[] args) {
-    AlgorithmAnalysis aa = new AlgorithmAnalysis();
-    System.out.println("actual number of steps:");
-    System.out.println(aa.first());
-    System.out.println(aa.second());
-    System.out.println(aa.third());
-    System.out.println(aa.fourth());
-    System.out.println(aa.fifth());
-    System.out.println(aa.sixth());
-    System.out.println(aa.seventh());
-  }
+	private int n = 100;
 
-  public int first() {
-    int sum = 0;
-    // Fragment #1
-    for ( int i = 0; i < n; i ++) {
-      sum++;
-    }
-    return sum;
-  }
+	public static void main(String[] args) {
+		AlgorithmAnalysis aa = new AlgorithmAnalysis();
+		aa.first(aa.n);
+		aa.second(aa.n);
+		aa.third(aa.n);
+		aa.fourth(aa.n);
+		aa.fifth(aa.n);
+		aa.sixth(aa.n);
+		aa.seventh(aa.n);
+	}
 
-  public int second() {
-    int sum = 0;
-    // Fragment #2
-    for ( int i = 0; i < n; i ++)
-      for ( int j = 0; j < n; j ++)
-        sum++;
-    return sum;
-  }
+	public void first(int n) {
+		int sum = 0;
+		System.out.println("Fragment 1:");
+		long start = System.nanoTime();
+		for (int i = 0; i < n; i++) {
+			sum++;
+		}
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime + "ns");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v -> O(n) (linear)
+		System.out.println("_____________________________");
 
-  public int third() {
-    int sum = 0;
-    // Fragment #3
-    for ( int i = 0; i < n; i ++)
-      for ( int j = i; j < n; j ++)
-        sum++;
-    return sum;
-  }
+	}
 
-  public int fourth() {
-    int sum = 0;
-    // Fragment #4
-    for ( int i = 0; i < n; i ++)
-      sum ++;
-    for ( int j = 0; j < n; j ++)
-      sum ++;
-    return sum;
-  }
+	public void second(int n) {
+		int sum = 0;
+		System.out.println("Fragment 2:");
+		long start = System.nanoTime();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				sum++;
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime/1000 + "ms");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v -> O(n^2) (quadratic)
+		System.out.println("_____________________________");
 
-  public int fifth() {
-    int sum = 0;
-    // Fragment #5
-    for ( int i = 0; i < n; i ++)
-      for ( int j = 0; j < n*n; j ++)
-        sum++;
-    return sum;
-  }
+	}
 
-  public int sixth() {
-    int sum = 0;
-    // Fragment #6
-    for ( int i = 0; i < n; i ++)
-      for ( int j = 0; j < i; j ++)
-        sum++;
-    return sum;
-  }
+	public void third(int n) {
+		int sum = 0;
+		System.out.println("Fragment 3:");
+		long start = System.nanoTime();
+		for (int i = 0; i < n; i++)
+			for (int j = i; j < n; j++)
+				sum++;
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime/1000 + "ms");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v ->  O(n^2)/2 + n/2 -> can be reduced to O(n^2)/2
+		System.out.println("_____________________________");
+	}
 
-  public int seventh() {
-    int sum = 0;
-    // Fragment #7
-    for ( int i = 1; i < n; i ++)
-      for ( int j = 0; j < n*n; j ++)
-        if (j % i == 0)
-          for (int k = 0; k < j; k++)
-            sum++;
-    return sum;
-  }
+	public void fourth(int n) {
+		int sum = 0;
+		System.out.println("Fragment 4:");
+		long start = System.nanoTime();
+		for (int i = 0; i < n; i++)
+			sum++;
+		for (int j = 0; j < n; j++)
+			sum++;
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime + "ns");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v -> O(2N)
+		System.out.println("_____________________________");
+	}
+
+	public void fifth(int n) {
+		int sum = 0;
+		System.out.println("Fragment 5:");
+		long start = System.nanoTime();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n * n; j++)
+				sum++;
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime/1000 + "ms");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v -> O(N^3)
+		System.out.println("_____________________________");
+	}
+
+	public void sixth(int n) {
+		int sum = 0;
+		System.out.println("Fragment 6:");
+		long start = System.nanoTime();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < i; j++)
+				sum++;
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime/1000 + "ms");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v -> less than O(n^2)/2 -n/2 -> can be reduced to O(n^2)/2  
+		System.out.println("_____________________________");
+	}
+
+	public void seventh(int n) {
+		int sum = 0;
+		System.out.println("Fragment 7:");
+		long start = System.nanoTime();
+		for (int i = 1; i < n; i++)
+			for (int j = 0; j < n * n; j++)
+				if (j % i == 0)
+					for (int k = 0; k < j; k++)
+						sum++;
+		long stop = System.nanoTime();
+		long runningTime = (stop - start);
+		System.out.println("Running Time: " + runningTime/1000 + "ms");
+		System.out.println("Operations: " + sum);
+		System.out.println("Input Size: " + n);
+		double v = (double) sum / n;
+		System.out.println("factor to multiply N: " + v); // that represent n*v -> O(N^5)
+		System.out.println("_____________________________");
+
+	}
 }
