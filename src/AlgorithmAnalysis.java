eec1c876229de781f16d8d4d95af9338650fddb7
disrupted <salomon.popp@gmail.com>
2017-05-14 03:49:15 +0200

    safe isPrime method

--- src/AlgorithmAnalysis.java
+++ src/AlgorithmAnalysis.java
@@ -11,8 +11,16 @@ public class AlgorithmAnalysis {
   }
 
   private static boolean isPrime(int n) {
-    return n > 1 && (n == 2 || n == 3 || (n * n - 1) % 24 == 0);
+    if (n < 2) return false;
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) return false;
+    }
+    return true;
   }
+
+  /* private static boolean isPrime(int n) {
+    return n > 1 && (n == 2 || n == 3 || (n * n - 1) % 24 == 0);
+  } */
 
   private void first(int n) {
     int sum = 0;
